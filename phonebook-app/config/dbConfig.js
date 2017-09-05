6263183d9dbc74052c634737c957e3b9d5e049cd
md khan <arifk36@yahoo.com>
2017-09-05 17:15:08 -0400

    set uo heroku

--- phonebook-app/config/dbConfig.js
+++ phonebook-app/config/dbConfig.js
@@ -1,7 +1,7 @@
 module.exports = process.env.DATABASE_URL || {
   host:     process.env.DB_HOST || 'localhost',
   port:     process.env.DB_PORT || 5432,
-  database: process.env.DB_NAME || 'phones_db_dev',
+  database: process.env.DATABASE_URL || 'phones_db_dev',
   /* user:     process.env.DB_USER, */
 };
 
